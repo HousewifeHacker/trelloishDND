9c4356900c9bfea9b3fbcb0d27f145e615143827
housewifehacker <housewifehacker@gmail.com>
2018-11-20 19:55:28 -0600

    Render data from a fake db of initial tasks state

--- trello-clone/src/index.js
+++ trello-clone/src/index.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import initialData from './initial-data';
 
-const App = () => 'Hello World!'
+class App extends Component {
+    state = initialData;
+
+    render() {
+        return this.state.columnOrder.map((columnId) => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+            return <Column key={column.id} column={column} tasks={tasks} />;
+        });
+    }
+}
+
+class Column extends Component {
+    render() {
+        return (
+            <div>
+                <h3>{this.props.column.title}</h3>
+                {this.props.tasks.map((task, index) => (
+                    <Task key={task.id} task={task} />
+                ))}
+            </div>
+        );
+    }
+}
+
+class Task extends Component {
+    render() {
+        return (
+            <div>
+                <h4>{this.props.task.title}</h4>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ trello-clone/src/initial-data.js
@@ -0,0 +1,30 @@
+/* JSON to fake DB */
+
+const initialData = {
+    tasks: {
+        'task-1': { id: 'task-1', title: 'Take out the garbage' },
+        'task-2': { id: 'task-2', title: 'Watch my favorite show' },
+        'task-3': { id: 'task-3', title: 'Charge my phone' },
+        'task-4': { id: 'task-4', title: 'Cook dinner' },
+    },
+    columns: {
+        'column-1': {
+            id: 'column-1',
+            title: 'To do',
+            taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+        },
+        'column-2': {
+            id: 'column-2',
+            title: 'Doing',
+            taskIds: [],
+        },
+        'column-3': {
+            id: 'column-3',
+            title: 'Done',
+            taskIds: [],
+        }
+    },
+    columnOrder: ['column-1', 'column-2', 'column-3'],
+}
+
+export default initialData;
